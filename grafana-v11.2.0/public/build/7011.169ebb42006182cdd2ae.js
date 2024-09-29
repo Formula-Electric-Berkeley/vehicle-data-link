645292ca548cd02690250c230aab5a8cc0710ef4
Viplove <viploverahate@gmail.com>
2024-09-29 12:58:51 -0700

    VDL: removed grafana api from git

--- grafana-v11.2.0/public/build/7011.169ebb42006182cdd2ae.js
+++
@@ -1,3 +0,0 @@
-"use strict";(self.webpackChunkgrafana=self.webpackChunkgrafana||[]).push([[7011],{67011:(u,n,a)=>{a.r(n),a.d(n,{default:()=>e});var r=a(74848),_=a(92762),s=a(71713);const e=({query:E})=>(0,r.jsx)("pre",{children:(0,r.jsx)(_.Z,{query:E,language:{grammar:s.Ay,name:"promql"}})})}}]);
-
-//# sourceMappingURL=7011.169ebb42006182cdd2ae.js.map
